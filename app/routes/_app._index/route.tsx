980c68db2f00e0c6edd0af1fc4d001e098a9c8aa
Qui Nguyen <quinguyen.dev@gmail.com>
2023-11-26 17:48:58 -0500

    init

---
+++ app/routes/_app._index/route.tsx
@@ -0,0 +1,19 @@
+import type { MetaFunction } from "@remix-run/node";
+
+// export const meta: MetaFunction = () => {
+//   return [
+//     { title: "Computer Consignment Shop" },
+//     {
+//       name: "description",
+//       content: "Welcome to our computer consignment shop!",
+//     },
+//   ];
+// };
+
+export default function AppIndex() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      This is the landing pagess
+    </div>
+  );
+}
---
+++ app/routes/_app/route.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { MetaFunction, Outlet } from "@remix-run/react";
+import Modal from "react-modal";
+import axios from "axios";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { NavigationBar } from "~/components";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Computer Consignment Shop" },
+    {
+      name: "description",
+      content: "Welcome to our computer consignment shop!",
+    },
+  ];
+};
+
+axios.defaults.baseURL =
+  "https://saqb4rb5je.execute-api.us-east-2.amazonaws.com/Initial";
+Modal.setAppElement("#root");
+
+const URL = "https://cs509-newegg.auth.us-east-2.amazoncognito.com";
+export default function AppLayout() {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NavigationBar />
+      <Outlet />
+    </QueryClientProvider>
+  );
+}
